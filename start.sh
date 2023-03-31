684a3f95eb43cab9307a628056918007b0a4cbbf
Philip Edekobi <philipedekobi@gmail.com>
2023-03-31 13:10:00 +0100

    letter casing for environment variables

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_Source" -verbose up
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
